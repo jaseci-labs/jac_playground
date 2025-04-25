01309cefab4c8d9d6fa396a5614add5e2b218f49
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-04-25 12:37:02 +0530

    Update routing in App component and change server port in Vite config

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/playground" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
