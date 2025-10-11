f478ba259de1f6a92d1f88556d226151da60327b
MalithaPrabhashana <malithaprabhashana@gmail.com>
2025-10-12 01:14:09 +0530

    Refactor comments for clarity in python.js and assetPaths.ts

--- src/lib/assetPaths.ts
+++ src/lib/assetPaths.ts
@@ -4,7 +4,7 @@
 // export const ONIGASM_WASM_PATH = "/playground/onigasm.wasm";
 // export const LANGUAGE_CONFIG_PATH = "/playground/language-configuration.json";
 
-// Normal Use
+// Development Use
 export const PYTHON_WORKER_PATH = "/python/python.js";
 export const JAC_EXAMPLES_JSON_PATH = "/jac_examples.json";
 export const ONIGASM_WASM_PATH = "/onigasm.wasm";
